3d577e593c691eb6faca879a589f8746aaaf6fa7
victorrequena20 <vrequena@sliscorp.com>
2023-11-16 07:03:39 -0400

    update

--- src/presentation/ui/pages/car/detailedCar/component/PreviewImage/index.tsx
+++ src/presentation/ui/pages/car/detailedCar/component/PreviewImage/index.tsx
@@ -1,10 +1,94 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "../zommedCar/ZoomedCarComponentStyles.scss";
 import Icons from "../../../../../assets/Icons";
 
 interface PreviewImageProps {
     imageUrl: string;
     close: () => void;
+}
+
+function ImageMagnifier({
+    src,
+    width,
+    height,
+    magnifierHeight = 200,
+    magnifieWidth = 200,
+    zoomLevel = 1.8,
+}: {
+    src: string;
+    width?: string;
+    height?: string;
+    magnifierHeight?: number;
+    magnifieWidth?: number;
+    zoomLevel?: number;
+}) {
+    const [[x, y], setXY] = useState([0, 0]);
+    const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
+    const [showMagnifier, setShowMagnifier] = useState(false);
+    return (
+        <div
+            style={{
+                position: "relative",
+                height: height,
+                width: width,
+            }}
+        >
+            <img
+                src={src}
+                style={{ height: height, width: width }}
+                onMouseEnter={(e) => {
+                    // update image size and turn-on magnifier
+                    const elem = e.currentTarget;
+                    const { width, height } = elem.getBoundingClientRect();
+                    setSize([width, height]);
+                    setShowMagnifier(true);
+                }}
+                onMouseMove={(e) => {
+                    // update cursor position
+                    const elem = e.currentTarget;
+                    const { top, left } = elem.getBoundingClientRect();
+
+                    // calculate cursor position on the image
+                    const x = e.pageX - left - window.pageXOffset;
+                    const y = e.pageY - top - window.pageYOffset;
+                    setXY([x, y]);
+                }}
+                onMouseLeave={() => {
+                    // close magnifier
+                    setShowMagnifier(false);
+                }}
+                alt={"img"}
+            />
+
+            <div
+                style={{
+                    display: showMagnifier ? "" : "none",
+                    position: "absolute",
+
+                    // prevent maginier blocks the mousemove event of img
+                    pointerEvents: "none",
+                    // set size of magnifier
+                    height: `${magnifierHeight}px`,
+                    width: `${magnifieWidth}px`,
+                    // move element center to cursor pos
+                    top: `${y - magnifierHeight / 2}px`,
+                    left: `${x - magnifieWidth / 2}px`,
+                    opacity: "1", // reduce opacity so you can verify position
+                    border: "1px solid lightgray",
+                    backgroundColor: "white",
+                    backgroundImage: `url('${src}')`,
+                    backgroundRepeat: "no-repeat",
+
+                    //calculate zoomed image size
+                    backgroundSize: `${imgWidth * zoomLevel}px ${imgHeight * zoomLevel}px`,
+
+                    //calculete position of zoomed image.
+                    backgroundPositionX: `${-x * zoomLevel + magnifieWidth / 2}px`,
+                    backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`,
+                }}
+            ></div>
+        </div>
+    );
 }
 
 const PreviewImage: FC<PreviewImageProps> = ({ imageUrl, close }) => {
@@ -15,7 +99,7 @@ const PreviewImage: FC<PreviewImageProps> = ({ imageUrl, close }) => {
             </div>
             <div className="zoom_car_component_image_container">
                 <div className="closer" onClick={close}></div>
-                <img src={imageUrl} alt="car" />
+                <ImageMagnifier width={"600px"} src={imageUrl} />
             </div>
         </div>
     );
