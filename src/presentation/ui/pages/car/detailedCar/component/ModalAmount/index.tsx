c7b1dc6662629329f3f5e8add2aad1413b8963fd
victorrequena20 <vrequena@sliscorp.com>
2023-11-21 16:08:03 -0400

    update

--- src/presentation/ui/pages/car/detailedCar/component/ModalAmount/index.tsx
+++ src/presentation/ui/pages/car/detailedCar/component/ModalAmount/index.tsx
@@ -5,6 +5,8 @@ import { useForm } from "react-hook-form";
 import di from "../../../../../../../di/DependencyInjection";
 import BookACarUseCase, { BookACarUseCaseName } from "../../../../../../../domain/use_cases/book/BookACarUseCase";
 import Validators from "../../../../../../utils/Validators";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 interface ModalAmountProps {
     close: () => void;
@@ -14,17 +16,27 @@ interface ModalAmountProps {
 
 type Inputs = {
     amount: number;
+    terms: boolean;
 };
 
 export default function ModalAmount({ close, id, carValue }: ModalAmountProps) {
-    console.log("🚀 ~ file: index.tsx:16 ~ ModalAmount ~ carValue:", carValue);
+    console.log("🚀 ~ file: index.tsx:23 ~ ModalAmount ~ carValue:", carValue);
+    const [acceptedTerms, setAcceptedTerms] = useState(false);
     const {
         register,
         setValue,
         handleSubmit,
         getValues,
+        watch,
         formState: { errors },
+        setError,
     } = useForm<Inputs>();
+    const terms = watch("terms");
+    const amount = watch("amount");
+
+    useEffect(() => {
+        console.log("TERMS_____", terms);
+    }, [terms]);
 
     const bookCar = async () => {
         function formatValue(value: string): string {
@@ -32,6 +44,16 @@ export default function ModalAmount({ close, id, carValue }: ModalAmountProps) {
             const formattedValue = plainValue + ".00";
             return formattedValue;
         }
+        console.log("AMOUNT____", formatValue(amount.toString()));
+        if (!terms) {
+            return;
+        }
+
+        if (parseFloat(formatValue(amount.toString().trimStart())) < 2000000.0) {
+            setError("amount", { message: "Minimo $2.000.000" });
+            return;
+        }
+
         try {
             const response = await window.fetch("https://api.wcaronline.com/api/scheduling-review/create/", {
                 method: "POST",
@@ -45,7 +67,7 @@ export default function ModalAmount({ close, id, carValue }: ModalAmountProps) {
             });
             const data = await response.json();
             // @ts-ignore
-            window.location.href = data?.data_eccolect?.eCollectUrl;
+            // window.location.href = data?.data_eccolect?.eCollectUrl;
         } catch (error) {
             console.log(error);
         }
@@ -67,9 +89,33 @@ export default function ModalAmount({ close, id, carValue }: ModalAmountProps) {
                             {...register("amount", Validators({ price: true, minValue: 2000000, maxValue: carValue }))}
                         />
                         <ErrorMessage as="aside" errors={errors} name="amount" />
+                        <div className="form-check mb-4 mt-4">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                onChange={() => {
+                                    console.log(acceptedTerms);
+                                    setAcceptedTerms(!acceptedTerms);
+                                    setValue("terms", !acceptedTerms);
+                                }}
+                                {...register("terms")}
+                            />
+                            <label className="form-check-label">
+                                <Link target="_blank" to={"/politicas-comprador"} className="text_underline text_gray">
+                                    Acepto términos y condiciones
+                                </Link>
+                            </label>
+                            <ErrorMessage as="aside" errors={errors} name="terms" />
+                        </div>
                     </div>
                     <div className="modal-buttons">
-                        <div className="btn btn_orange btn-modal" onClick={bookCar}>
+                        <div
+                            className="btn btn_orange btn-modal"
+                            style={{
+                                backgroundColor: getValues().terms ? "" : "#ccc",
+                            }}
+                            onClick={bookCar}
+                        >
                             <p>Apartar</p>
                         </div>
                         <div className="btn btn_orange_outline" onClick={close}>
