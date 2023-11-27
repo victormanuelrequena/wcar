8d674ebfd2963f13f3b245b13123b9942ca2d295
victorrequena20 <vrequena@sliscorp.com>
2023-11-27 16:38:48 -0400

    remove mocked image

--- src/presentation/ui/components/carCard/CarCardComponent.tsx
+++ src/presentation/ui/components/carCard/CarCardComponent.tsx
@@ -13,7 +13,6 @@ import FavoriteCarsContextType from "../../../../domain/providers/favoriteCars/F
 import { getUrlCar } from "../../../utils/Contants";
 
 const CarCardComponent: FC<CarCardComponentProps> = ({ car }) => {
-    console.log("🚀 ~ file: CarCardComponent.tsx:16 ~ car:", car);
     const [_like, _setLike] = useState<boolean>(false);
     const { favoriteCars } = useContext(FavoriteCarsContext) as FavoriteCarsContextType;
 
@@ -38,8 +37,9 @@ const CarCardComponent: FC<CarCardComponentProps> = ({ car }) => {
             to={`${routes.detailedCar.relativePath}/${getUrlCar(car)}`}
             className="w-100 card car_card_component pb-2"
         >
+            {/* // src={parseInt(car.id) === 162 ? "/assets/car-mocked.jpeg" : car.photoUrl} */}
             <img
-                src={parseInt(car.id) === 162 ? "/assets/car-mocked.jpeg" : car.photoUrl}
+                src={car.photoUrl}
                 alt={`${car.name} ${car.brand.name} ${car.type.name}`}
                 title="Wcar"
                 className="img-fluid img_car"
