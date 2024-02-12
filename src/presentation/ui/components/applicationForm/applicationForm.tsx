b4bba61150c290dd3e8be29d99a51646f570fbe4
Jesusqr-11v <jesus.aqr2003@gmail.com>
2024-02-12 14:59:37 -0400

    cambios

--- src/presentation/ui/components/applicationForm/applicationForm.tsx
+++ src/presentation/ui/components/applicationForm/applicationForm.tsx
@@ -1,34 +1,79 @@
 import "./applicationFormStyles.scss";
+import { useForm } from "react-hook-form";
 
 export const ApplicationForm = ({ setOpenForm, setOpenModal, setAlert }) => {
+    const { register, handleSubmit } = useForm();
+    const url = "https://api.wcaronline.com/api";
+
+    const onSubmit = handleSubmit((data) => {
+        console.log(data);
+
+        fetch(`${url}/video-assistances/create/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then(() => {
+                setOpenForm(false);
+                setAlert(true);
+            })
+            .catch((e) => console.error(e));
+    });
+
     return (
         <>
             <div className="application_form text-white">
-                <form className="form">
+                <form className="form" onSubmit={onSubmit}>
                     <p className="title">Formulario de solicitud</p>
                     <div className="input_container form-group">
                         <label htmlFor="name" className="label_input mandatory">
                             Nombre
                         </label>
-                        <input type="text" id="name" placeholder="nombre" className="input form-control" />
+                        <input
+                            type="text"
+                            id="name"
+                            placeholder="nombre"
+                            className="input form-control"
+                            {...register("name")}
+                        />
                     </div>
                     <div className="input_container form-group">
                         <label htmlFor="last_name" className="label_input mandatory">
                             Apellido
                         </label>
-                        <input type="text" id="last_name" placeholder="apellido" className="input form-control" />
+                        <input
+                            type="text"
+                            id="last_name"
+                            placeholder="apellido"
+                            className="input form-control"
+                            {...register("last_name")}
+                        />
                     </div>
                     <div className="input_container form-group">
                         <label htmlFor="phone" className="label_input mandatory">
                             Teléfono
                         </label>
-                        <input type="text" id="phone" placeholder="número de teléfono" className="input form-control" />
+                        <input
+                            type="text"
+                            id="phone"
+                            placeholder="número de teléfono"
+                            className="input form-control"
+                            {...register("phone")}
+                        />
                     </div>
                     <div className="input_container form-group">
                         <label htmlFor="email" className="label_input mandatory">
                             Email
                         </label>
-                        <input type="email" id="email" placeholder="ejemplo@gmail.com" className="input form-control" />
+                        <input
+                            type="email"
+                            id="email"
+                            placeholder="ejemplo@gmail.com"
+                            className="input form-control"
+                            {...register("email")}
+                        />
                     </div>
                     <div className="botton">
                         <button
@@ -41,15 +86,7 @@ export const ApplicationForm = ({ setOpenForm, setOpenModal, setAlert }) => {
                         >
                             CANCELAR
                         </button>
-                        <button
-                            className="ms-3 btn btn_orange"
-                            onClick={() => {
-                                setAlert(true);
-                                setOpenForm(false);
-                            }}
-                        >
-                            CONTINUAR
-                        </button>
+                        <button className="ms-3 btn btn_orange">CONTINUAR</button>
                     </div>
                 </form>
             </div>
